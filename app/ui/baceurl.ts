81ada8afdf7b67fc3a1c64771b8538291d456451
Ec4you_Digital <devteam@ec4you.in>
2026-08-26 15:51:48 +0530

    Switch BASE_URL from localhost to production URL
    base URL add

--- app/ui/baceurl.ts
+++ app/ui/baceurl.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://core.reluxelectric.com";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://core.reluxelectric.com";
+// export const BASE_URL = "http://localhost:8000";
 export const SITE_URL = "https://reluxelectric.com";
